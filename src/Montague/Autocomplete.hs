a32d4287f1c06d825c0056338f46b53d1ae5ef82
Nathan Bedell <nbedell@tulane.edu>
2021-08-21 20:53:16 -0400

    Add signature for autocomplete module.

--- src/Montague/Autocomplete.hs
+++ src/Montague/Autocomplete.hs
@@ -1,3 +1,11 @@
 
-module Montague.Autocomplete where
+module Montague.Autocomplete(
+  getAutocomplete
+) where
 
+import Montague
+
+-- | Take an annotated term, and return a list of possible atoms 
+-- that could be added to the right to produce a new production.
+getAutocomplete :: MontagueSemantics a t x => AnnotatedTerm a t -> [a]
+getAutocomplete current = undefined
