e3cd5e59cf07f58c99f8acc0d143d99cdd631546
Nathan Bedell <nbedell@tulane.edu>
2022-03-18 21:59:15 -0400

    Refactor MontagueSemantics to only require typeOf on atoms.

--- src/Montague/Semantics.hs
+++ src/Montague/Semantics.hs
@@ -6,7 +6,11 @@ import Montague.Types
 -- | Typeclass defining an interpretation of the atom type a and
 -- base types t in the type x.
 data MontagueSemantics a t x = MontagueSemantics {
-    typeOf    :: Term a t -> MontagueType t,
-    parseTerm :: String -> MontagueTerm a t,
-    interp    :: AnnotatedTerm a t -> x
+    typeOfAtom :: a -> MontagueType t,
+    parseTerm  :: String -> MontagueTerm a t,
+    interp     :: AnnotatedTerm a t -> x
 }
+
+typeOf :: MontagueSemantics a t x -> Term t a -> MontagueType t
+typeOf semantics (Atom x) = typeOfAtom x
+typeOf semantics _ = undefined
