ffa13d01f307dea0e8032dc84f54ac885c804ced
Nathan Bedell <nbedell@tulane.edu>
2022-07-04 15:33:42 -0400

    Fix nix files.

--- default.nix
+++ default.nix
@@ -1,14 +1,2 @@
-{ mkDerivation, base, lib, monad-tree, parsec, partial-order
-, prettyprinter
-}:
-mkDerivation {
-  pname = "montague";
-  version = "0.0.1.0";
-  src = ./.;
-  libraryHaskellDepends = [
-    base monad-tree parsec partial-order prettyprinter
-  ];
-  description = "Non-deterministic parser for natural languages";
-  license = "unknown";
-  hydraPlatforms = lib.platforms.none;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8107" }:
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./montague.nix { }
