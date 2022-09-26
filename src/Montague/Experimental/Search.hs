9a2b92377cee761f7d2f93e0e7748165342fb901
Nathan Bedell <nbedell@tulane.edu>
2022-09-25 21:51:04 -0600

    Add basic definitions in the search module.

--- src/Montague/Experimental/Search.hs
+++ src/Montague/Experimental/Search.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE ExistentialQuantification #-}
 
 module Montague.Experimental.Search where
+import Data.Row
+import Montague.Experimental.Typed
+
+type RuleHead _Ω xs = Row xs -> Term _Ω _Ω
+type RuleBody _Ω xs = Row xs -> [Term _Ω _Ω]
+
+data Rule _Ω = forall xs. Rule (RuleHead _Ω xs) (RuleBody _Ω xs)
 
