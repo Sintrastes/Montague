b5a478e003f16d4e57a771e0133beae60aee0bfa
Nathan Bedell <nbedell@tulane.edu>
2022-09-25 19:53:55 -0600

    Add a new datatype for Kripke semantics.

--- src/Montague/Experimental/Worlds.hs
+++ src/Montague/Experimental/Worlds.hs
@@ -9,3 +9,9 @@ type TBool = Int -> Bool
 -- | "Graded" boolean type -- truth values
 -- over possible worlds w.
 type GBool w = w -> Bool
+
+-- | The data for a computational representation of Kripke semantics,
+-- consisting of the current world, and a relation (represented as a function
+-- with multiple return values) representing "related worlds"
+data Kripke w = Kripke w (w -> [w])
+
