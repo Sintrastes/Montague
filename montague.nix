ac206b137c86039b884ac97628e2c94698baec9b
Nathan Bedell <nbedell@tulane.edu>
2022-09-25 21:54:19 -0600

    Update montague.nix.

--- montague.nix
+++ montague.nix
@@ -1,12 +1,12 @@
 { mkDerivation, base, lib, monad-tree, parsec, partial-order
-, prettyprinter
+, prettyprinter, row-types, monad-coroutine
 }:
 mkDerivation {
   pname = "montague";
   version = "0.0.1.0";
   src = ./.;
   libraryHaskellDepends = [
-    base monad-tree parsec partial-order prettyprinter
+    base monad-tree parsec partial-order prettyprinter row-types monad-coroutine
   ];
   description = "Non-deterministic parser for natural languages";
   license = "unknown";
