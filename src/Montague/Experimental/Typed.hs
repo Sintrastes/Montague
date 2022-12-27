1c216f79f31da3e321d2e44cd8a6c38b4c4722c1
Nathan Bedell <nbedell@tulane.edu>
2022-12-26 23:13:28 -0500

    Remove unused typed variant of terms.

--- src/Montague/Experimental/Typed.hs
+++ src/Montague/Experimental/Typed.hs
@@ -1,5 +1,6 @@
 
-{-# LANGUAGE ScopedTypeVariables, TypeOperators, DataKinds, KindSignatures, TypeApplications, TypeFamilies, GADTs, FlexibleInstances, FlexibleContexts, MultiParamTypeClasses, LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables, TypeOperators, DataKinds, KindSignatures, TypeApplications, 
+   TypeFamilies, GADTs, FlexibleInstances, FlexibleContexts, MultiParamTypeClasses, LambdaCase #-}
 
 module Montague.Experimental.Typed where
 
@@ -38,17 +39,17 @@ instance Show LambekType where
 type Sentence = 'T Bool
 
 class BooleanType _Ω a where
-  coord :: (LambekTerm _Ω (T _Ω) -> LambekTerm _Ω (T _Ω) -> LambekTerm _Ω (T _Ω)) 
-    -> LambekTerm _Ω a -> LambekTerm _Ω a -> LambekTerm _Ω a
+  coord :: (Term _Ω (T _Ω) -> Term _Ω (T _Ω) -> Term _Ω (T _Ω)) 
+    -> Term _Ω a -> Term _Ω a -> Term _Ω a
 
 instance BooleanType _Ω (T _Ω) where
   coord f x y = f x y
 
 instance BooleanType _Ω b => BooleanType _Ω (L b a) where
-  coord f x y = LLamL $ \v -> coord f (LAppL x v) (LAppL y v)
+  coord f x y = LamL $ \v -> coord f (AppL x v) (AppL y v)
 
 instance BooleanType _Ω b => BooleanType _Ω (R a b) where
-  coord f x y = LLamR $ \v -> coord f (LAppR v x) (LAppR v y)
+  coord f x y = LamR $ \v -> coord f (AppR v x) (AppR v y)
 
 -- | Type family to get the semantic interpretation of a lambek
 -- type.
@@ -59,21 +60,26 @@ type family Bracket (x :: LambekType) :: Type where
   Bracket (a ∧ b)  = (Bracket a, Bracket b)
   Bracket (a ∨ b)  = Either (Bracket a) (Bracket b)
 
--- | A type of terms abstracted over a type of "truth values"
--- _Ω.
+-- | A type of terms in the lambek calculus abstracted over a type
+-- of "truth values" _Ω.
 --
 -- The symbol Ω was chosen to evoke the similarity of the 
 --  subobject classifier in topos theory.
 data Term _Ω a where
-    Lam  :: (Term _Ω a -> Term _Ω b) -> Term _Ω (a -> b)
-    App  :: Term _Ω (a -> b) -> Term _Ω a -> Term _Ω b
-    Atom :: Typeable a => a -> Term _Ω a
-    And  :: Term _Ω _Ω -> Term _Ω _Ω -> Term _Ω _Ω
-    Or   :: Term _Ω _Ω -> Term _Ω _Ω -> Term _Ω _Ω
-    All  :: (Term _Ω a    -> Term _Ω _Ω) -> Term _Ω _Ω
-    Some :: (Term _Ω _Ω -> Term _Ω _Ω) -> Term _Ω _Ω
-    Will :: Term _Ω _Ω -> Term _Ω _Ω
-    WillAlways :: Term _Ω _Ω -> Term _Ω _Ω
+    LamL  :: (Term _Ω b -> Term _Ω a) -> Term _Ω (L a b)
+    LamR  :: (Term _Ω a -> Term _Ω b) -> Term _Ω (R a b)
+    AppL  :: Term _Ω (b / a) -> Term _Ω a -> Term _Ω b
+    AppR  :: Term _Ω a -> Term _Ω (a \\ b) -> Term _Ω b
+    Atom  :: (Show a, Typeable a) => a -> Term _Ω (T a)
+    And   :: Term _Ω (T _Ω) -> Term _Ω (T _Ω) -> Term _Ω (T _Ω)
+    Or    :: Term _Ω (T _Ω) -> Term _Ω (T _Ω) -> Term _Ω (T _Ω)
+    All   :: (Term _Ω a -> Term _Ω (T _Ω)) -> Term _Ω (T _Ω)
+    Some  :: (Term _Ω (T _Ω) -> Term _Ω (T _Ω)) 
+       -> Term _Ω (T _Ω)
+    Var   :: String -> Proxy a -> Term _Ω a
+
+-- | The world is the totality of facts, not of things. -- Wittgenstein
+type Facts _Ω = [Term _Ω (T _Ω)]
 
 data SomeTerm _Ω = forall a. Typeable a => SomeTerm (Term _Ω a)
 
@@ -87,57 +93,42 @@ substVar x y t = undefined
 unify :: Term _Ω a -> Term _Ω a -> [(String, SomeTerm _Ω)]
 unify = undefined
 
--- | The world is the totality of facts, not of things. -- Wittgenstein
-type Facts _Ω w = [Term _Ω (w -> Bool)]
-
-data LambekTerm _Ω a where
-    LLamL  :: (LambekTerm _Ω b -> LambekTerm _Ω a) -> LambekTerm _Ω (L a b)
-    LLamR  :: (LambekTerm _Ω a -> LambekTerm _Ω b) -> LambekTerm _Ω (R a b)
-    LAppL  :: LambekTerm _Ω (b / a) -> LambekTerm _Ω a -> LambekTerm _Ω b
-    LAppR  :: LambekTerm _Ω a -> LambekTerm _Ω (a \\ b) -> LambekTerm _Ω b
-    LAtom  :: (Show a, Typeable a) => a -> LambekTerm _Ω (T a)
-    LAnd   :: LambekTerm _Ω (T _Ω) -> LambekTerm _Ω (T _Ω) -> LambekTerm _Ω (T _Ω)
-    LOr    :: LambekTerm _Ω (T _Ω) -> LambekTerm _Ω (T _Ω) -> LambekTerm _Ω (T _Ω)
-    LAll   :: (LambekTerm _Ω a -> LambekTerm _Ω (T _Ω)) -> LambekTerm _Ω (T _Ω)
-    LSome  :: (LambekTerm _Ω (T _Ω) -> LambekTerm _Ω (T _Ω)) 
-       -> LambekTerm _Ω (T _Ω)
-    LVar   :: String -> Proxy a -> LambekTerm _Ω a
-
-instance (Show _Ω) => Show (LambekTerm _Ω a) where
+instance (Show _Ω) => Show (Term _Ω a) where
   show = \case
-    LLamL f   -> "λₗx."  ++ (show $ f (LVar "x" Proxy)) -- TODO: Make sure variables are not captured here.
-    LLamR f   -> "λᵣx." ++ (show $ f (LVar "x" Proxy)) -- TODO: Make sure variables are not captured here.
-    LAppL f x -> (show f) ++ " " ++ (show x)
-    LAppR f x -> (show f) ++ " " ++ (show x)
-    LAtom x   -> show x
-    LAnd x y  -> (show x) ++ " ∧ " ++ (show y)
-    LOr x y   -> (show x) ++ " ∨ " ++ (show y)
-    LAll f    -> "∀x." ++ (show $ f (LVar "x" Proxy)) -- TODO: Make sure variables are not captured here.
-    LSome f   -> "∃x." ++ (show $ f (LVar "x" Proxy)) -- TODO: Make sure variables are not captured here.
+    LamL f   -> "λₗx."  ++ (show $ f (Var "x" Proxy)) -- TODO: Make sure variables are not captured here.
+    LamR f   -> "λᵣx." ++ (show $ f (Var "x" Proxy)) -- TODO: Make sure variables are not captured here.
+    AppL f x -> (show f) ++ " " ++ (show x)
+    AppR f x -> (show f) ++ " " ++ (show x)
+    Atom x   -> show x
+    And x y  -> (show x) ++ " ∧ " ++ (show y)
+    Or x y   -> (show x) ++ " ∨ " ++ (show y)
+    All f    -> "∀x." ++ (show $ f (Var "x" Proxy)) -- TODO: Make sure variables are not captured here.
+    Some f   -> "∃x." ++ (show $ f (Var "x" Proxy)) -- TODO: Make sure variables are not captured here.
 
-instance Eq a => Eq (LambekTerm _Ω (T a)) where
-  (LAtom x) == (LAtom y) = x == y
+instance Eq a => Eq (Term _Ω (T a)) where
+  (Atom x) == (Atom y) = x == y
   _ == _ = False
 
 -- | "Type-raising" operator. Converts a type a to a type b/(a\b).
 -- Useful for making non-boolean types boolean for the sake of coordination.
-raise :: LambekTerm _Ω a -> LambekTerm _Ω (b/(a\\b))
-raise x = LLamL $ \(v :: LambekTerm _Ω (a\\b)) -> LAppR x v
+raise :: Term _Ω a -> Term _Ω (b/(a\\b))
+raise x = LamL $ \(v :: Term _Ω (a\\b)) -> AppR x v
 
 -- | Function ("proof") witnessing the associativity of the lambek
 --  calculus.
-assoc :: LambekTerm _Ω ((a \\ b) / c) -> LambekTerm _Ω (a \\ (b / c))
-assoc x = LLamR $ \y -> LLamL $ \z -> y `LAppR` (x `LAppL` z)
+assoc :: Term _Ω ((a \\ b) / c) -> Term _Ω (a \\ (b / c))
+assoc x = LamR $ \y -> LamL $ \z -> y `AppR` (x `AppL` z)
 
 -- | Function ("proof") witnessing the associativity of the lambek
 --  calculus in the opposite direction as assoc.
-unassoc :: LambekTerm _Ω (a \\ (b / c)) -> LambekTerm _Ω ((a \\ b) / c)
-unassoc x = LLamL $ \z -> LLamR $ \y -> (y `LAppR` x) `LAppL` z
+unassoc :: Term _Ω (a \\ (b / c)) -> Term _Ω ((a \\ b) / c)
+unassoc x = LamL $ \z -> LamR $ \y -> (y `AppR` x) `AppL` z
 
 -- Example from SEP:
-every :: Term _Ω ((a -> _Ω) -> (a -> _Ω) -> _Ω)
-every = Lam $ \p -> Lam $ \q -> All $ \x ->
-  And (App p x) (App q x)
+--  Needs to be re-worked with the approach from Carpenter.
+-- every :: Term _Ω ((a -> (T _Ω)) (T _Ω) (a -> (T _Ω))
+-- every = Lam $ \p -> Lam $ \q -> All $ \x ->
+--  And (App p x) (App q x)
 
 data Person = Nate | William | Michael | Andrew deriving(Eq, Show)
 
@@ -148,33 +139,33 @@ data Person = Nate | William | Michael | Andrew deriving(Eq, Show)
 -- I guess we could have the set of "facts" be part of the "world",
 -- and recursively reference (maybe via an implicit parameter)
 -- the search procedure in light of the current set of rules.
-likes :: LambekTerm Bool ((T Person \\ T Bool) / T Person)
-likes = LLamL $ \x -> LLamR $ \y -> LAtom $
+likes :: Term Bool ((T Person \\ T Bool) / T Person)
+likes = LamL $ \x -> LamR $ \y -> Atom $
     x == nate && y == william || x == william && y == nate
 
--- nate :: LambekTerm _Ω (T Person)
-nate = LAtom Nate
+-- nate :: Term _Ω (T Person)
+nate = Atom Nate
 
--- william :: LambekTerm _Ω (T Person)
-william = LAtom William
+-- william :: Term _Ω (T Person)
+william = Atom William
 
-michael = LAtom Michael
+michael = Atom Michael
 
--- example :: LambekTerm _Ω (T _Ω)
-example = nate `LAppR` (likes `LAppL` william)
+-- example :: Term _Ω (T _Ω)
+example = nate `AppR` (likes `AppL` william)
 
 -- Coordination
-example2 = (nate `LAppR` (likes `LAppL` william))
+example2 = (nate `AppR` (likes `AppL` william))
     `Montague.Experimental.Typed.and` 
-     (william `LAppR` (likes `LAppL` nate))
+     (william `AppR` (likes `AppL` nate))
 
 -- Coordination via type-raising.
 example3 = raise nate
    `Montague.Experimental.Typed.and` raise william
-   `LAppL` (likes `LAppL` michael)
+   `AppL` (likes `AppL` michael)
 
 -- | -ed morpeme: play-ed -> played. am-ed -> was. see-ed -> saw.
-ed :: Term _Ω _Ω -> Term _Ω _Ω
+ed :: Term _Ω (T _Ω) -> Term _Ω (T _Ω)
 ed = undefined
 
 -- | "will" -- a future tense modal operator over possible worlds.
@@ -182,16 +173,16 @@ ed = undefined
 -- In terms of Kripke semantics, this is the "possibility" operator
 --  over the "future worlds" relation.
 --
-will :: Term _Ω _Ω -> Term _Ω _Ω
-will = Will
+-- will :: Term _Ω (T _Ω) -> Term _Ω (T _Ω)
+-- will = Will
 
 -- | "will always" -- a future tense modal operator over possible worlds.
 -- 
 -- In terms of Kripke semantics, this is the "nescesity" operator
 --  over the "future worlds" relation.
 --
-willAlways :: Term _Ω _Ω -> Term _Ω _Ω
-willAlways = WillAlways
+-- willAlways :: Term _Ω (T _Ω) -> Term _Ω (T _Ω)
+-- willAlways = WillAlways
 
 -- | Here is what an evaluation function for our algebra could look like.
 -- We can try to evaluate universal and existential propositions,
@@ -199,14 +190,14 @@ willAlways = WillAlways
 -- 
 -- Returns a coroutine which can be run indefinitely to continue producing 
 -- intermediate results.
-eval :: Term _Ω _Ω -> Coroutine (Yield Bool) Identity Bool
+eval :: Term _Ω (T _Ω) -> Coroutine (Yield Bool) Identity Bool
 eval = undefined
 
-and :: BooleanType _Ω a => LambekTerm _Ω a -> LambekTerm _Ω a -> LambekTerm _Ω a
-and x y = coord LAnd x y
+and :: BooleanType _Ω a => Term _Ω a -> Term _Ω a -> Term _Ω a
+and x y = coord And x y
 
-or :: BooleanType _Ω a => LambekTerm _Ω a -> LambekTerm _Ω a -> LambekTerm _Ω a
-or x y = coord LOr x y
+or :: BooleanType _Ω a => Term _Ω a -> Term _Ω a -> Term _Ω a
+or x y = coord Or x y
 
 -- | Get the current (partial) truth value from a coroutine.
 currentValue :: Coroutine (Yield Bool) Identity Bool -> Prop4
