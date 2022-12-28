2e38ba1b2c50c74c1b0c36bf57b1b98cee28b45a
Nathan Bedell <nbedell@tulane.edu>
2022-12-28 10:28:14 -0500

    Add evaluation utilities.

--- src/Montague/Experimental/Typed.hs
+++ src/Montague/Experimental/Typed.hs
@@ -18,16 +18,16 @@ import Data.Functor.Identity
 import Control.Monad.Coroutine.SuspensionFunctors (Yield)
 import Montague.Experimental.LambekType
 import Data.List
-import Control.Exception (AllocationLimitExceeded)
+import Debug.Trace
 
 type Sentence = 'T Bool
 
 class BooleanType _Ω a where
-  coord :: (_Ω -> _Ω -> _Ω) 
+  coord :: (Var _Ω -> Var _Ω -> _Ω) 
     -> Term a -> Term a -> Term a
 
 instance BooleanType _Ω (T _Ω) where
-  coord f (Atom x) (Atom y) = Atom $ f x y
+  coord f (Atom x) (Atom y) = Atom $ Val $ f x y
 
 instance BooleanType _Ω b => BooleanType _Ω (L b a) where
   coord f x y = LamL $ \v -> coord f (x <. v) (y <. v)
@@ -79,8 +79,38 @@ data Term a where
     AppE  :: Term (a ↑ b) -> Term b -> Term a
     -- Also not sure if this is right or not.
     AppS  :: Term (b ⇑ a) -> (Term b -> Term a) -> Term a
-    Atom  :: (Show a, Typeable a) => a -> Term (T a)
-    TVar   :: String -> Proxy a -> Term a
+    Atom  :: (Show a, Typeable a) => Var a -> Term (T a)
+    TVar  :: String -> Term a
+
+toplevel = \case
+  LamL _   -> "LamL"
+  LamR _   -> "LamR"
+  LamE _   -> "LamE"
+  LamS _   -> "LamS"
+  LamN _   -> "LamN"
+  AppL _ _ -> "AppL"
+  AppR _ _ -> "AppR"
+  AppE _ _ -> "AppE"
+  AppS _ _ -> "AppS"
+  Atom _   -> "Atom"
+  TVar _   -> "TVar"
+
+
+-- | Evaluate a lambda expression to normal form by beta reduction.
+eval :: Term a -> Term a
+eval (AppL (LamL f) x) = eval $ f x
+eval (AppR x (LamR f)) = eval $ f x
+eval (AppR x f) = eval $ AppR (eval x) (eval f)
+eval (AppE (LamE f) x) = eval $ f x
+eval (AppS (LamS f) x) = eval $ f x
+eval (AppS f x) = eval $ AppS (eval f) (eval <$> x)
+eval x = x
+
+evalAtom :: Show a => Typeable a => Term (T a) -> Term (T a)
+evalAtom x = evalAtom' $ eval x
+
+evalAtom' (TVar x) = Atom $ Var x
+evalAtom' x = x
 
 (<.) = AppL
 (.>) = AppR
@@ -88,17 +118,24 @@ data Term a where
 -- | An example type of raw semantic expressions.
 data Sem where
     Pred       :: String -> [Sem] -> Sem
-    Individual :: Person -> Sem
-    Subject    :: Subject -> Sem
-    And        :: Sem -> Sem -> Sem
-    Or         :: Sem -> Sem -> Sem
-    Implies    :: Sem -> Sem -> Sem
-    All        :: (a -> Sem) -> Sem
-    Exists     :: (a -> Sem) -> Sem
+    Individual :: Var Person -> Sem
+    Subject    :: Var Subject -> Sem
+    And        :: Var Sem -> Var Sem -> Sem
+    Or         :: Var Sem -> Var Sem -> Sem
+    Implies    :: Var Sem -> Var Sem -> Sem
+    All        :: (Var a -> Sem) -> Sem
+    Exists     :: (Var a -> Sem) -> Sem
     Not        :: Sem -> Sem
-    Var        :: String -> Sem
     The        :: Sem -> Sem
     Const      :: Bool -> Sem
+
+data Var a = Var String | Val a
+
+instance Show a => Show (Var a) where
+  show (Var x) = x
+  show (Val x) = show x
+
+getVar (Val x) = x
 
 -- | Semantics for the definite determiner.
 ι = The
@@ -120,24 +157,33 @@ unify = undefined
 
 instance Show (Term a) where
   show = \case
-    LamL f   -> "λₗx."  ++ show (f (TVar "x" Proxy)) -- TODO: Make sure variables are not captured here.
-    LamR f   -> "λᵣx." ++ show (f (TVar "x" Proxy)) -- TODO: Make sure variables are not captured here.
-    AppL f x -> show f ++ " " ++ show x
-    AppR f x -> show f ++ " " ++ show x
+    LamL f   -> "λₗx."  ++ show (f (TVar "x")) -- TODO: Make sure variables are not captured here.
+    LamR f   -> "λᵣx." ++ show (f (TVar "x")) -- TODO: Make sure variables are not captured here.
+    LamN f   -> "λx." ++ show (f (TVar "x"))
+    LamE f   -> "λx." ++ show (f (TVar "x"))
+    LamS f   -> "λx.S" -- ++ show (f (TVar "x" Proxy))
+    AppL f x -> show f ++ "(" ++ show x ++ ")"
+    AppR f x -> show f ++ "(" ++ show x ++ ")"
+    AppS x f -> show (f (TVar "x")) ++ "(" ++ show x ++ ")"
+    AppE f x -> show f ++ "(" ++ show x ++ ")"
     Atom x   -> show x
+    TVar x   -> x
 
 instance Show Sem where
     show = \case
       Pred p xs -> p ++ "(" ++ intercalate ", " (show <$> xs) ++ ")"
-      Var x -> x
       And x y   -> show x ++ " ∧ " ++ show y
       Or x y    -> show x ++ " ∨ " ++ show y
-      All f     -> "∀x." -- ++ show (f (Var "x")) -- TODO: Make sure variables are not captured here.
-      Exists f  -> "∃x." -- ++ show (f (Var "x")) -- TODO: Make sure variables are not captured here.
+      Implies x y -> show x ++ " → " ++ show y
+      All f     -> "∀x." ++ show (f (Var "x")) -- TODO: Make sure variables are not captured here.
+      Exists f  -> "∃x." ++ show (f (Var "x")) -- TODO: Make sure variables are not captured here.
       The x     -> "ι(" ++ show x ++ ")"
+      Individual x -> show x
+      Subject x    -> show x
+      Const x -> show x
 
 instance Eq a => Eq (Term (T a)) where
-  (Atom x) == (Atom y) = x == y
+  (Atom (Val x)) == (Atom (Val y)) = x == y
   _ == _ = False
 
 -- | "Type-raising" operator. Converts a type a to a type b/(a\b).
@@ -158,17 +204,6 @@ assoc x = LamR $ \y -> LamL $ \z -> y .> (x <. z)
 --  calculus in the opposite direction as assoc.
 unassoc :: Term (a \\ (b / c)) -> Term ((a \\ b) / c)
 unassoc x = LamL $ \z -> LamR $ \y -> (y .> x) <. z
-
--- | Variant of unassoc for type-raised arguments.
--- except I don't think this makes sense at all...
--- unassoc' :: Term (a / (b \\ c)) -> Term ((a / b) \\ c) 
--- unassoc' x = LamR $ \y -> y
-
--- Example from SEP:
---  Needs to be re-worked with the approach from Carpenter.
--- every :: Term _Ω ((a -> (T _Ω)) (T _Ω) (a -> (T _Ω))
--- every = Lam $ \p -> Lam $ \q -> All $ \x ->
---  And (App p x) (App q x)
 
 class HasAttributes n a | n -> a where
 
@@ -209,8 +244,8 @@ data Subject =
     | Hiking
   deriving(Eq, Show)
 
-cooking = Atom Cooking
-anime = Atom Anime
+cooking = Atom $ Val Cooking
+anime = Atom $ Val Anime
 
 -- | Here we can define a term in terms of a concrete semantics,
 -- but how do we define a term that can depend on the current list of
@@ -220,12 +255,12 @@ anime = Atom Anime
 -- and recursively reference (maybe via an implicit parameter)
 -- the search procedure in light of the current set of rules.
 likes :: Term ((NP Person \\ S Sem) / NP Person)
-likes = LamL $ \(Atom x) -> LamR $ \(Atom y) -> Atom $
+likes = LamL $ \(Atom x) -> LamR $ \(Atom y) -> Atom $ Val $
     Pred "likes" [Individual x, Individual y]
 
 -- | Likes, in the sense of a person and a subject.
 likes2 :: Term ((NP Person \\ S Sem) / NP Subject)
-likes2 = LamL $ \(Atom x) -> LamR $ \(Atom y) -> Atom $
+likes2 = LamL $ \(Atom x) -> LamR $ \(Atom y) -> Atom $ Val $
     Pred "likes" [Individual y, Subject x]
 
 -- | Definite determiner -- converts a noun into a
@@ -236,7 +271,7 @@ the (LamN x) = undefined -- Atom (ι x)
 -- | An example of a noun which is not a proper noun.
 man :: Term (N Person Sem)
 man = LamN $ \x -> 
-  Atom $ Const True
+  Atom $ Val $ Const True
 
 -- who (or which, since we do not distinguish animacy), used to construct relative
 --  clauses.
@@ -246,7 +281,7 @@ who = LamL $ \v -> LamR $ \p -> LamN $ \x -> let
      LamN p' = p
      Atom a2 = p' x
   in
-     Atom $ And a1 a2 
+     Atom $ Val $ And a1 a2
 
 relativeClauseExample :: Term (S Sem ↑ NP Person)
 relativeClauseExample = LamE $ \x ->
@@ -267,51 +302,54 @@ relativeClauseUsage2 = the $
         .> (raise' cooking `and'` raise' anime)))
 
 nate :: Term (NP Person)
-nate = Atom Nate
+nate = Atom $ Val Nate
 
 william :: Term (NP Person)
-william = Atom William
+william = Atom $ Val William
 
-michael = Atom Michael
+michael = Atom $ Val Michael
 
-socrates = Atom Socrates
+socrates = Atom $ Val Socrates
 
 class Entity n o | n -> o where
-  inj :: n -> o
+  inj :: Var n -> o
 
 instance Entity Person Sem where
   inj = Individual
 
 -- | The copula, in the sense of attribution.
-is :: Show a => Entity n Sem => HasAttributes n a => Term (NP n \\ S Sem / T a)
-is = LamL $ \attr -> LamR $ \(Atom entity) -> 
-  Atom $ Pred (show attr) [inj entity]
+is :: Typeable n => Show n => Show a => Entity n Sem => HasAttributes n a => Term (NP n \\ S Sem / T a)
+is = LamL $ \attr -> LamR $ \e -> let Atom entity = evalAtom e in
+  Atom $ Val $ Pred (show attr) [inj entity]
 
 every :: Typeable n => Show n => Term ((NP n ⇑ S Sem) / N n Sem)
-every = LamL $ \(LamN p) -> LamS $ \q -> Atom $
-    All $ \(x :: n) -> let 
-      Atom p1 = p (Atom x)
-      Atom p2 = q (Atom x)
+every = LamL $ \p' -> LamS $ \q -> Atom $ Val $ 
+    All $ \x -> let 
+      LamN p = p'
+      e1 = evalAtom $ p (Atom x)
+      e2 = evalAtom $ q (Atom x)
+      Atom p1 = e1
+      Atom p2 = e2
     in 
       p1 `Implies` p2
 
 some :: Typeable n => Show n => Term ((NP n ⇑ S Sem) / N n Sem)
-some = LamL $ \(LamN p) -> LamS $ \q -> Atom $
-    Exists $ \(x :: n) -> let 
-      Atom p1 = p (Atom x)
-      Atom p2 = q (Atom x)
+some = LamL $ \(LamN p) -> LamS $ \q -> Atom $ Val $
+    Exists $ \(x :: Var n) -> let 
+      Atom p1 = eval $ p (Atom x)
+      Atom p2 = eval $ q (Atom x)
     in
       p1 `And` p2
 
 no :: Typeable n => Show n => Term ((NP n ⇑ S Sem) / N n Sem)
-no = LamL $ \(LamN p) -> LamS $ \q -> Atom $
-    Exists $ \(x :: n) -> let 
-      Atom p1 = p (Atom x)
-      Atom p2 = q (Atom x)
+no = LamL $ \(LamN p) -> LamS $ \q -> Atom $ Val $ 
+    Exists $ \(x :: Var n) -> let 
+      Atom p1 = eval $ p (Atom x)
+      Atom p2 = eval $ q (Atom x)
     in
       Not $ p1 `And` p2
 
-mortal = Atom Mortal
+mortal = Atom $ Val Mortal
 
 -- syllogism1 = socrates .> is <. a man
 
